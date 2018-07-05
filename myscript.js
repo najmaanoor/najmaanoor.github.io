653b860e43eeb6b3e75890fc33e038228bf5456c
Dhara Panchal <panchal.dhara17@gmail.com>
2018-07-05 10:22:37 -0400

    first commit

---
+++ myscript.js
@@ -0,0 +1,10 @@
+function addinfo() {
+    document.getElementById("box1").innerHTML = "<h3><left> Hi! My name is Najma. I am 16 years old.</left></h3>"
+    document.getElementById("box1").innerHTML = "<div><h3><left> Apart from being the Vice President of my school's FBLA,</left></h3></div>"
+    document.getElementById("box1").innerHTML = "<div><h3><left> I am also a Girls Who Code Summer Immersion Program student and </left></h3></div>"
+    document.getElementById("box1").innerHTML = "<div><h3><left> a performer in Clarkston High School's International Show </left></h3></div>"
+    document.getElementById("box1").innerHTML = "<div><h3><left> with the Somali Dancers.</left></h3></div>"
+
+
+
+}
